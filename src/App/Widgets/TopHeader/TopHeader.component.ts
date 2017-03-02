a542651c5ad02cbf68486ea12a2af65670bf74bf
Mocha-- <xibowangmelb@gmail.com>
2017-03-02 11:41:11 +1100

    add(widget): add top header

---
+++ src/App/Widgets/TopHeader/TopHeader.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import './TopHeader.component.styl';
+
+@Component({
+    selector: 'top-header',
+    templateUrl: './TopHeader.component.html'
+})
+export class TopHeader {
+
+}
--- src/App/Widgets/Widgets.module.ts
+++ src/App/Widgets/Widgets.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { TodoItem } from './TodoItem/TodoItem.component';
 import { TodoList } from './TodoList/TodoList.component';
 import { NewTodo } from './NewTodo/NewTodo.component';
+import { TopHeader } from './TopHeader/TopHeader.component';
 
 @NgModule({
     imports: [CommonModule, FormsModule],
-    declarations: [TodoList, TodoItem, NewTodo],
-    exports: [TodoList, TodoItem, NewTodo]
+    declarations: [TodoList, TodoItem, NewTodo, TopHeader],
+    exports: [TodoList, TodoItem, NewTodo, TopHeader]
 })
 export class WidgetsModule {}
